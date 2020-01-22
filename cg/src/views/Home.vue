a1d98c2485b52656fdd5b83c2890f9795f5cca95
Ismael López Larumbe <ismaello125@gmail.com>
2020-01-22 00:02:05 -0600

    Mensaje de Prueba en Home.vue

--- cg/src/views/Home.vue
+++ cg/src/views/Home.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Nombre: Ismael Lopez Larumbe"/>
+    <HelloWorld msg="Prueba de PUSH"/>
   </div>
 </template>
 
