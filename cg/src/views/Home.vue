d615e93d9a93cae74fa7c770ffd1da41f130cc34
Ismael López Larumbe <ismaello125@gmail.com>
2020-01-21 23:56:39 -0600

    Aqui se empieza rama css

--- cg/src/views/Home.vue
+++ cg/src/views/Home.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <HelloWorld msg="Nombre: Ismael Lopez Larumbe"/>
   </div>
 </template>
 
