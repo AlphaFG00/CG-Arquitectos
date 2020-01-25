ed51eb34b02fdb2af4c4152ec6c9af7897b5c375
AlphaFG00 <drummfer99@gmail.com>
2020-01-24 23:30:55 -0600

    aca estan las vistas larumbe :v

---
+++ cg/src/views/Proyectos.vue
@@ -0,0 +1,10 @@
+<template>
+  <div class="about">
+    <h1>Estos son los Proyectos </h1>
+  </div>
+</template>
+<script>
+export default {
+  name:"Proyectos"
+}
+</script>
---
+++ cg/src/views/Servicios.vue
@@ -0,0 +1,6 @@
+<template>
+  <div class="about">
+    <h1>Estos son los Servicios</h1>
+    <h2>HEY</h2>
+  </div>
+</template>
