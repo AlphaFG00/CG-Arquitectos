a1641ae72fb41752945cb811fb716edfe873d6af
Ismael López Larumbe <ismaello125@gmail.com>
2020-04-06 23:00:37 -0500

    Agregado el 6*6 carrucel de proyectos

--- cg/src/components/ListaProyectos.vue
+++ cg/src/components/ListaProyectos.vue
@@ -1,46 +1,104 @@
 <template>
     <div class="lista-proyectos">
-      <b-container fluid>
-        <b-row class="contenedor">
-          <b-col cols="12" md="6" lg="4" class="thumbex">
-            <div class="thumbnail" @click="showModel(0)">
-                <b-img fluid :src="getImgUrl(images[0].name)" :alt="images[0].name" class="img-proyecto"></b-img>
-                <span>{{images[0].title}}</span>
-            </div>
-          </b-col>
-          <b-col cols="12" md="6" lg="4" class="thumbex">
-            <div class="thumbnail" @click="showModel(1)">
-                <b-img fluid :src="getImgUrl(images[1].name)" :alt="images[1].name" class="img-proyecto"></b-img>
-                <span>{{images[1].title}}</span>
-            </div>
-          </b-col>
-          <b-col cols="12" md="6" lg="4" class="thumbex">
-            <div class="thumbnail" @click="showModel(2)">
-                <b-img fluid :src="getImgUrl(images[2].name)" :alt="images[2].name" class="img-proyecto"></b-img>
-                <span>{{images[2].title}}</span>
-            </div>
-          </b-col>
-          <b-col cols="12" md="6" lg="4" class="thumbex">
-            <div class="thumbnail" @click="showModel(3)">
-                <b-img fluid :src="getImgUrl(images[3].name)" :alt="images[3].name" class="img-proyecto"></b-img>
-                <span>{{images[3].title}}</span>
-            </div>
-          </b-col>
-          <b-col cols="12" md="6" lg="4" class="thumbex">
-            <div class="thumbnail" @click="showModel(4)">
-                <b-img fluid :src="getImgUrl(images[4].name)" :alt="images[4].name" class="img-proyecto"></b-img>
-                <span>{{images[4].title}}</span>
-            </div>
-          </b-col>
-          <b-col cols="12" md="6" lg="4" class="thumbex">
-            <div class="thumbnail" @click="showModel(5)">
-                <b-img fluid :src="getImgUrl(images[5].name)" :alt="images[5].name" class="img-proyecto"></b-img>
-                <span>{{images[5].title}}</span>
-            </div>
-          </b-col>
-        </b-row>
-      </b-container>
-      <div>
+        <b-carousel
+                id="carousel-1"
+                v-model="slide"
+                :interval="4000"
+                controls
+                indicators
+                background="#ababab"
+                img-width="1024"
+                img-height="512"
+                @sliding-start="onSlideStart"
+                @sliding-end="onSlideEnd">
+        <b-carousel-slide img-blank>
+          <template>
+            <!--Saca las imagenes y nombres de images del store-->
+            <b-row class="contenedor">
+              <b-col cols="12" md="6" lg="4" class="thumbex">
+                <div class="thumbnail" @click="showModel(0)">
+                    <b-img fluid :src="getImgUrl(images[0].name)" :alt="images[0].name" class="img-proyecto"></b-img>
+                    <span>{{images[0].title}}</span>
+                </div>
+              </b-col>
+              <b-col cols="12" md="6" lg="4" class="thumbex">
+                <div class="thumbnail" @click="showModel(1)">
+                    <b-img fluid :src="getImgUrl(images[1].name)" :alt="images[1].name" class="img-proyecto"></b-img>
+                    <span>{{images[1].title}}</span>
+                </div>
+              </b-col>
+              <b-col cols="12" md="6" lg="4" class="thumbex">
+                <div class="thumbnail" @click="showModel(2)">
+                    <b-img fluid :src="getImgUrl(images[2].name)" :alt="images[2].name" class="img-proyecto"></b-img>
+                    <span>{{images[2].title}}</span>
+                </div>
+              </b-col>
+              <b-col cols="12" md="6" lg="4" class="thumbex">
+                <div class="thumbnail" @click="showModel(3)">
+                    <b-img fluid :src="getImgUrl(images[3].name)" :alt="images[3].name" class="img-proyecto"></b-img>
+                    <span>{{images[3].title}}</span>
+                </div>
+              </b-col>
+              <b-col cols="12" md="6" lg="4" class="thumbex">
+                <div class="thumbnail" @click="showModel(4)">
+                    <b-img fluid :src="getImgUrl(images[4].name)" :alt="images[4].name" class="img-proyecto"></b-img>
+                    <span>{{images[4].title}}</span>
+                </div>
+              </b-col>
+              <b-col cols="12" md="6" lg="4" class="thumbex">
+                <div class="thumbnail" @click="showModel(5)">
+                    <b-img fluid :src="getImgUrl(images[5].name)" :alt="images[5].name" class="img-proyecto"></b-img>
+                    <span>{{images[5].title}}</span>
+                </div>
+              </b-col>
+            </b-row>
+          </template>
+        </b-carousel-slide>
+          <!--Pack de 6-->
+        <b-carousel-slide  img-blank img-alt="Blank image">
+          <template>
+            <b-row class="contenedor">
+              <b-col cols="12" md="6" lg="4" class="thumbex">
+                <div class="thumbnail" @click="showModel(0)">
+                    <b-img fluid :src="getImgUrl(images[0].name)" :alt="images[0].name" class="img-proyecto"></b-img>
+                    <span>{{images[0].title}}</span>
+                </div>
+              </b-col>
+              <b-col cols="12" md="6" lg="4" class="thumbex">
+                <div class="thumbnail" @click="showModel(1)">
+                    <b-img fluid :src="getImgUrl(images[1].name)" :alt="images[1].name" class="img-proyecto"></b-img>
+                    <span>{{images[1].title}}</span>
+                </div>
+              </b-col>
+              <b-col cols="12" md="6" lg="4" class="thumbex">
+                <div class="thumbnail" @click="showModel(2)">
+                    <b-img fluid :src="getImgUrl(images[2].name)" :alt="images[2].name" class="img-proyecto"></b-img>
+                    <span>{{images[2].title}}</span>
+                </div>
+              </b-col>
+              <b-col cols="12" md="6" lg="4" class="thumbex">
+                <div class="thumbnail" @click="showModel(3)">
+                    <b-img fluid :src="getImgUrl(images[3].name)" :alt="images[3].name" class="img-proyecto"></b-img>
+                    <span>{{images[3].title}}</span>
+                </div>
+              </b-col>
+              <b-col cols="12" md="6" lg="4" class="thumbex">
+                <div class="thumbnail" @click="showModel(4)">
+                    <b-img fluid :src="getImgUrl(images[4].name)" :alt="images[4].name" class="img-proyecto"></b-img>
+                    <span>{{images[4].title}}</span>
+                </div>
+              </b-col>
+              <b-col cols="12" md="6" lg="4" class="thumbex">
+                <div class="thumbnail" @click="showModel(5)">
+                    <b-img fluid :src="getImgUrl(images[5].name)" :alt="images[5].name" class="img-proyecto"></b-img>
+                    <span>{{images[5].title}}</span>
+                </div>
+              </b-col>
+            </b-row>
+            </template>
+        </b-carousel-slide>
+        </b-carousel>
+    <div>
         <ModalAltitude :proyectTitle="images[0].title" :ref="0"/>
         <ModalApaches :proyectTitle="images[1].title" :ref="1"/>
         <ModalFinox :proyectTitle="images[2].title" :ref="2"/>
@@ -61,6 +119,12 @@ import ModalLeonDavid from '@/components/ModalLeonDavid.vue'
 import ModalLasAguilas from '@/components/ModalLasAguilas.vue'
 export default {
     name:'ListaProyectos',
+    data(){
+      return {
+       slide: 0,
+       sliding: null
+      }
+    },
     components:{
           ModalAltitude,ModalApaches,ModalFinox,ModalHuasteca,ModalLeonDavid,ModalLasAguilas
     },
@@ -75,6 +139,12 @@ export default {
         showModel(index){
          var mod=this.$refs[index].$refs.myModal
          mod.classList.add("show-modal");
+        },
+        onSlideStart(slide) {
+          this.sliding = true
+        },
+        onSlideEnd(slide) {
+          this.sliding = false
         }
     }
 }
@@ -82,6 +152,10 @@ export default {
 </script>
 
 <style scoped>
+.img-fluid{
+  display: block;
+  height: 400px;
+}
   /*css - tabla de proyectos */
 .contenedor {
   display: flex;
@@ -110,6 +184,7 @@ export default {
 .contenedor>.thumbex>.thumbnail .img-proyecto {
   z-index: 1;
   min-height: 200px;
+  max-height: 200px;
 }
 
 .contenedor>.thumbex>.thumbnail span{
@@ -138,5 +213,6 @@ export default {
 }
 .contenedor>.thumbex>.thumbnail:hover span {
   opacity: 0;
+
 }
 </style>
