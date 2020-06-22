7763757ecbed966362852edc898e48f86391fa1e
srchach <gsikmo@gmail.com>
2020-06-22 18:32:48 -0500

    REFACTOR: Refactored code. Added SASS helpers.

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -8,19 +8,6 @@
   </div>
 </template>
 
-<script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
-export default {
-  name: 'home',
-  components:{
-    HelloWorld
-  }
- 
-}
-</script>
-
 <style scoped>
 .home{
     width: auto;
