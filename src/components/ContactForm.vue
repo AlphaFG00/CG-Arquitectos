87b7f9172b225fd463ff78cc15a5fb616dfff2f2
Ismael López Larumbe <ismaello125@gmail.com>
2020-07-02 07:25:42 -0500

    FEATURE: base view of Contact

---
+++ src/components/ContactForm.vue
@@ -0,0 +1,122 @@
+<template>
+    <div class="ContactForm">
+    <b-form @submit="onSubmit" @reset="onReset" v-if="show">
+
+        <b-form-group id="input-group-1" label="Nombre:" label-for="input-1">
+            <b-form-input
+            id="input-1"
+            v-model="form.name"
+            required
+            placeholder="Enter name"
+            >
+            </b-form-input>
+        </b-form-group>
+
+        <b-form-group id="input-group-2" label="Correo Electrónico:" label-for="input-2">
+            <b-form-input
+            id="input-2"
+            v-model="form.email"
+            type="email"
+            required
+            placeholder="Ingrese su correo"
+            >
+            </b-form-input>
+        </b-form-group>
+
+        <b-form-group
+            id="input-group-3"
+            label="Número de teléfono:"
+            label-for="input-3"
+            description="Sus datos no seran compartidos ni expuestos a terceros."
+        >
+            <b-form-input
+            id="input-3"
+            v-model="form.number"
+            type="text"
+            required
+            placeholder="Ingrese su correo"
+            >
+            </b-form-input>
+        </b-form-group>
+
+        <b-form-group id="input-group-4" label="Mensaje:" label-for="input-4">
+            <b-form-textarea
+            id="input-4"
+            v-model="form.text"
+            required
+            placeholder="Mensaje"
+            >
+            </b-form-textarea>
+        </b-form-group>
+
+        <b-button type="submit" variant="primary">Enviar</b-button>
+        <b-button type="reset" variant="danger">Limpiar</b-button>
+    </b-form>
+    </div>
+</template>
+<script>
+  export default {
+    name: 'ContactForm',
+    data() {
+      return {
+        form: {
+          email: '',
+          name: '',
+          number: '',
+          text: ''
+        },
+        show: true
+      }
+    },
+    methods: {
+      onSubmit(evt) {
+        evt.preventDefault()
+        alert(JSON.stringify(this.form))
+      },
+      onReset(evt) {
+        evt.preventDefault()
+        // Reset our form values
+        this.form.email = ''
+        this.form.name = ''
+        this.form.text = ''
+        this.form.number=''
+        // Trick to reset/clear native browser form validation state
+        this.show = false
+        this.$nextTick(() => {
+          this.show = true
+        })
+      }
+    }
+  }
+</script>
+
+<style lang="scss" scoped>
+$sizeInBlock: 80%;
+$sizeOfLaterals:10%;
+/*sizeInBlock + 2(sizeOfLaterals) has to be equal to 100% */
+    .ContactForm{
+        margin-top: 10px;
+        padding-bottom: 5px;
+        padding-top: 5px;
+        max-width: $sizeInBlock;
+        margin-right: $sizeOfLaterals/2;
+        margin-left: $sizeOfLaterals/2;
+        padding-right: $sizeOfLaterals/2;
+        padding-left: $sizeOfLaterals/2;
+        border: solid 2px rgb(219, 196, 132);
+        font-size: .8em;
+        .form-group{
+            margin-bottom: 5px;
+            text-align: left;
+        }
+        .form-control{font-size:.9em}
+        button{
+            font-size: .9em;
+            color: black;
+            padding:3px 10px;
+            margin: 0px 10px;
+            background-color: white;
+            border-color:rgb(219, 196, 132);
+        }
+    }
+</style>
--- src/views/Contacto.vue
+++ src/views/Contacto.vue
@@ -3,21 +3,42 @@
     <b-container id="view-contacto">
     <!--Estructura para el efecto del titulo INICIO-->
       <div class="Titulo-Cuerpo">
-        <div class="titulo-letra">Conectate</div>
+        <div class="titulo-letra">Cotactate</div>
         <div class="titulo-restante">
           <pre><span>con Nosotros</span></pre>
         </div>
       </div>
     <!--Estructura para el efecto del titulo Final-->
-      <div class="Texto-contacto">
-
+      <div>
+        <b-row class="ViewCuerpo" align-v="center">
+          <b-col cols="12" md="6" lg="6">
+            <div class="persona-contacto">
+              <span>Arq. Rogelio Cázares Rodríguez</span>
+              <span><i class="fas fa-phone-square icon-phone"></i><span>Cel. 8119783811</span></span>
+              <span><i class="fas fa-envelope icon-email"></i><span>rogelio.cazares@garcadesarrollos.com</span></span>
+            </div>
+            <div class="persona-contacto">
+              <span>Lic. Elisa García</span>
+              <span><i class="fas fa-phone-square icon-phone"></i><span>Cel. 8112505670</span></span>
+              <span><i class="fas fa-envelope icon-email"></i><span>elisa.garcia@garcadesarrollos.com</span></span>
+            </div>
+          </b-col>
+          <b-col cols="12" md="6" lg="6">
+            <div class="Mapa">
+              <span class="direccion">CRT. NACIONAL KM 262. LOS CRISTALES, MONTERREY, NUEVO LEÓN, MÉXICO CP. 64985</span>
+              <div style="width=100%; background-color: royalblue; height:200px;">*MAPA*</div>
+            </div>
+          </b-col>
+        </b-row>
       </div>
+    <ContactForm/>
     </b-container>
   </div>
 </template>
 
-<style scoped>
- .Contacto{
+<style scoped lang="scss">
+
+.Contacto{
     width: auto;
     min-height: 100vh;
     padding-bottom: 10px;
@@ -42,7 +63,6 @@
   animation-name: slideinright;
   margin-top: 15px;
   margin-bottom: 10px;
-
   }
   .Titulo-Cuerpo pre{ /*texto demaciado grande, se nesecita pre para evitar modificaciones de html*/
     margin: 0;
@@ -73,58 +93,58 @@
     animation: slidein 5s .4s forwards;
   }
 
-@keyframes showup {
-  0% {
-    opacity: 0;
-    margin-right: 100px;
+  @keyframes showup {
+    0% {
+      opacity: 0;
+      margin-right: 100px;
+    }
+    10% {
+      opacity: 1;
+      margin-right: 0px;
+    }
+    80% {
+      opacity: 1;
+    }
+    100% {
+      opacity: 1;
+    }
   }
-  10% {
-    opacity: 1;
-    margin-right: 0px;
-  }
-  80% {
-    opacity: 1;
-  }
-  100% {
-    opacity: 1;
-  }
-}
 
-@keyframes slidein {
-  0% {
-    margin-left: -800px;
+  @keyframes slidein {
+    0% {
+      margin-left: -800px;
+    }
+    10% {
+      margin-left: -800px;
+    }
+    30% {
+      margin-left: 0px;
+    }
+    100% {
+      margin-left: 0px;
+    }
   }
-  10% {
-    margin-left: -800px;
-  }
-  30% {
-    margin-left: 0px;
-  }
-  100% {
-    margin-left: 0px;
-  }
-}
 
-@keyframes reveal {
-  0% {
-    opacity: 0;
-    width: 0px;
+  @keyframes reveal {
+    0% {
+      opacity: 0;
+      width: 0px;
+    }
+    10% {
+      opacity: 1;
+      width: 0px;
+    }
+    25% {
+      width: 200px;
+    }
+    80% {
+      opacity: 1;
+    }
+    100% {
+      opacity: 1;
+      width: 200px;
+    }
   }
-  10% {
-    opacity: 1;
-    width: 0px;
-  }
-  25% {
-    width: 200px;
-  }
-  80% {
-    opacity: 1;
-  }
-  100% {
-    opacity: 1;
-    width: 200px;
-  }
-}
   @keyframes slideinright {
     from {
       margin-left: 100%;
@@ -135,4 +155,49 @@
       width: 100%;
     }
   }
+/*Fin del css por default */
+
+$icons-size: 2em;
+$tam-celda: 100%;
+.ViewCuerpo{
+  .persona-contacto{
+    border: solid 1px rgb(219, 196, 132);
+    text-align: left;
+    >span{/*Hijos directos*/
+      margin-left: 3%;
+      display: block;
+      font-family:Arial, Helvetica, sans-serif;
+      font-size: .9em;
+    }
+    >span .icon-email,>span .icon-phone{
+      font-size: $icons-size;
+      margin-right: 5%;
+    }
+  }
+  .persona-contacto:first-child{
+    margin-bottom: 20px;
+  }
+  .Mapa{
+    border: solid 1px rgb(219, 196, 132);
+    .direccion{
+      display: block;
+      width: 100%;
+      font-family:Arial, Helvetica, sans-serif;
+      font-size: .8em;
+    }
+  }
+}
+.icon-phone{color: rgb(32, 39, 131) !important;}
+.icon-email{color: rgb(187, 39, 28) !important;}
+
 </style>
+
+<script>
+import ContactForm from '@/components/ContactForm.vue';
+export default {
+  name: 'Contacto',
+  components:{
+    ContactForm
+  }
+}
+</script>
