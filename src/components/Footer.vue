8597f68c8f7bddfcbc8264f3abebdc9c300b8907
Ismael López Larumbe <ismaello125@gmail.com>
2020-07-06 09:41:00 -0500

    FIX: wrong facebook deleted, title width of proyectos changed.

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -14,7 +14,7 @@
         <a href="https://twitter.com/_cgarquitectura" target="_blank">
           <i class="fab fa-twitter-square color-tw"></i>
         </a>
-        <a href="https://www.facebook.com/garcadesarrollos/" target="_blank">
+        <a href="" target="_blank">
           <i class="fab fa-facebook-square color-fb"></i>
         </a>
         <a href="https://api.WhatsApp.com/send?phone=+5218119783811" target="_blank">
